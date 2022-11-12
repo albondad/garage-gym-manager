d76a691d73fd6071c94c846d98efa964cd234723
Al Bondad <albondad@gmail.com>
2022-11-12 00:36:11 -0600

    Set up firebase service

--- packages/rest-api/src/app.service.ts
+++ packages/rest-api/src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { FirebaseService } from './firebase/firebase.service';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  onModuleInit() {
+    FirebaseService.start();
   }
 }
