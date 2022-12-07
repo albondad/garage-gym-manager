e54daf1642ed44cf3731989d8942bd75c7ffef73
Al Bondad <albondad@gmail.com>
2022-12-06 21:22:11 -0600

    Set up NavigationBar component

---
+++ packages/web/components/navigation-bar/navigation-bar.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+
+export const NavigationBar = () => {
+  return (
+    <Box
+      alignItems="center"
+      backgroundColor="#000000"
+      color="#ffffff"
+      display="flex"
+      height="4rem"
+    >
+      test
+    </Box>
+  );
+};
