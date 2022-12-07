bbffe967b406a488fee904e6d50a952a4b6f0980
Al Bondad <albondad@gmail.com>
2022-12-06 21:28:08 -0600

    Set up poppins font

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `Poppins`,
+    body: `Poppins`,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
